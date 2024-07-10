061da4bfd4300e6a03b7f993572984d4808301a1
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-10 11:07:27 +0530

    Update clean_faltfile.py

--- clean_faltfile.py
+++ clean_faltfile.py
@@ -8,3 +8,29 @@ data = pd.read_csv("vt_tax_data_2016.csv", dtype=data_types)
 
 # Print data types of resulting frame
 print(data.dtypes.head())
+
+
+"""Null Values"""
+
+# Create dict specifying that 0s in zipcode are NA values
+null_values = {'zipcode':0}
+
+# Load csv using na_values keyword argument
+data = pd.read_csv("vt_tax_data_2016.csv", 
+                   na_values=null_values)
+
+# View rows with NA ZIP codes
+print(data[data.zipcode.isna()])
+
+"""Files line aand error"""
+try:
+  # Set warn_bad_lines to issue warnings about bad records
+  data = pd.read_csv("vt_tax_data_2016_corrupt.csv", 
+                     error_bad_lines=False, 
+                     warn_bad_lines=True)
+  
+  # View first 5 records
+  print(data.head())
+  
+except pd.errors.ParserError:
+    print("Your data contained rows that could not be parsed.")
