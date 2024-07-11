3974c48f25aebc3cd3e87f70ab78f1eff4800df9
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-12 00:15:31 +0530

    Update excel_file.py

--- excel_file.py
+++ excel_file.py
@@ -10,3 +10,22 @@ survey_responses = pd.read_excel("fcc_survey_headers.xlsx",
 
 # View the names of the columns selected
 print(survey_responses.columns)
+
+
+
+"""Excel File Reading DataFrame"""
+
+# Create an empty dataframe
+all_responses = pd.DataFrame()
+
+# Set up for loop to iterate through values in responses
+for df in responses.values():
+  # Print the number of rows being added
+  print("Adding {} rows".format(df.shape[0]))
+  # Concatenate all_responses and df, assign result
+  all_responses = pd.concat([all_responses,df])
+
+# Graph employment statuses in sample
+counts = all_responses.groupby("EmploymentStatus").EmploymentStatus.count()
+counts.plot.barh()
+plt.show(
