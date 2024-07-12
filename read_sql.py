5def25dda977946f3edfc98ba7a02705cf949161
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-12 11:56:01 +0530

    Update read_sql.py

--- read_sql.py
+++ read_sql.py
@@ -46,3 +46,21 @@ wintry_days = pd.read_sql(query,engine)
 
 # View summary stats about the temperatures
 print(wintry_days.describe())
+
+
+"""Group by clause"""
+
+# Create query to get call counts by complaint_type
+query = """
+SELECT complaint_type, 
+     count(*)
+  FROM hpd311calls
+  GROUP BY complaint_type;
+"""
+
+# Create dataframe of call counts by issue
+calls_by_issue = pd.read_sql(query, engine)
+
+# Graph the number of calls for each housing issue
+calls_by_issue.plot.barh(x="complaint_type")
+plt.show()
