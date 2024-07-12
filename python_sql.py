09c126356e5ae771909ae335de5049c24669c073
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-12 10:33:48 +0530

    Create python_sql.py

---
+++ python_sql.py
@@ -0,0 +1,9 @@
+"""create engine in pythong"""
+# Import sqlalchemy's create_engine() function
+from sqlalchemy import create_engine
+
+# Create the database engine
+engine = create_engine('sqlite:///data.db')
+
+# View the tables in the database
+print(engine.table_names())
