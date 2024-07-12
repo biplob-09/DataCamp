9e29b03bc4887a5b072d5cdd99d3e2ec5da26e61
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-12 11:04:08 +0530

    Update read_sql.py

--- read_sql.py
+++ read_sql.py
@@ -14,3 +14,18 @@ weather = pd.read_sql(query, engine)
 
 # View the first few rows of data
 print(weather.head())
+
+
+"""Read the entire weather table using the select *"""
+
+# Create the database engine
+engine = create_engine("sqlite:///data.db")
+
+# Create a SQL query to load the entire weather table
+query =""" SELECT * FROM weather""";
+
+# Load weather with the SQL query
+weather = pd.read_sql(query, engine)
+
+# View the first few rows of data
+print(weather.head())
