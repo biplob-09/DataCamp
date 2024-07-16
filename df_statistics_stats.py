ae7e8596fa9f9bf0c04d7aa6eaea73e5025b4a6c
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-16 12:52:36 +0530

    Update df_statistics_stats.py

--- df_statistics_stats.py
+++ df_statistics_stats.py
@@ -15,3 +15,18 @@ def iqr(column):
 
 # Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
 print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, np.median]))
+
+
+-----------------------------
+
+# Sort sales_1_1 by date
+sales_1_1 = sales_1_1.sort_values("date",ascending=True)
+
+# Get the cumulative sum of weekly_sales, add as cum_weekly_sales col
+sales_1_1["cum_weekly_sales"] = sales_1_1["weekly_sales"].cumsum()
+
+# Get the cumulative max of weekly_sales, add as cum_max_sales col
+sales_1_1["cum_max_sales"] = sales_1_1["weekly_sales"].cummax()
+
+# See the columns you calculated
+print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
