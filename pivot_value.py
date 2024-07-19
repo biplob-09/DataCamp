a4f7ab64f1784b463a84ed026bb35e7d74d96433
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-19 13:02:29 +0530

    Update pivot_value.py

--- pivot_value.py
+++ pivot_value.py
@@ -5,3 +5,14 @@ mean_sales_by_type = sales.pivot_table(values="weekly_sales",index="type")
 
 # Print mean_sales_by_type
 print(mean_sales_by_type)
+
+
+"""ex2"""
+# Import NumPy as np
+import numpy as np
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales",index='type', aggfunc=[np.mean,np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
