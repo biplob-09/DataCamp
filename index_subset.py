28c09456b531568b3c01698522c8da4ec6d3bef2
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-07-23 12:49:29 +0530

    Update index_subset.py

--- index_subset.py
+++ index_subset.py
@@ -26,3 +26,15 @@ print(temperatures[temperatures["city"].isin(cities)])
 
 # Subset temperatures_ind using .loc[]
 print(temperatures_ind.loc[cities])
+
+
+"""Index level and sorting """
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level=["city"]))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=["country","city"],ascending = [True,False]))
