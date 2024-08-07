8d5761e5b6dfe0db728fab6bfd0bed5e12b8f8e0
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-08-07 21:25:59 +0530

    Update merging_tables.py

--- merging_tables.py
+++ merging_tables.py
@@ -58,3 +58,16 @@ titles_diff = orig_seq[['title_org','title_seq','diff']]
 
 # Print the first rows of the sorted titles_diff
 print(titles_diff.sort_values('diff',ascending=False).head())
+
+"""Inner Join"""
+ Merge the non_mus_tcks and top_invoices tables on tid
+tracks_invoices = non_mus_tcks.merge(top_invoices, on='tid')
+
+# Use .isin() to subset non_mus_tcks to rows with tid in tracks_invoices
+top_tracks = non_mus_tcks[non_mus_tcks['tid'].isin(tracks_invoices['tid'])]
+
+# Group the top_tracks by gid and count the tid rows
+cnt_by_gid = top_tracks.groupby(['gid'], as_index=False).agg({'tid':'count'})
+
+# Merge the genres table to cnt_by_gid on gid and print
+print(cnt_by_gid.merge(genres,on='gid'))
