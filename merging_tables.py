3f631380966ede7cbe6d7d80ab66b87a3e14d756
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-08-07 14:25:45 +0530

    Update merging_tables.py

--- merging_tables.py
+++ merging_tables.py
@@ -21,3 +21,22 @@ sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant','account','pop_2010'],
 
 # Print the top few rows of sorted_pop_vac_lic
 print(sorted_pop_vac_lic.head())
+
+
+"""Merging using left on and right on
+Most of the recent big-budget science fiction movies can also be classified as action movies. You are given a table of science fiction movies called scifi_movies and another table of action movies called action_movies. Your goal is to find which movies are considered only science fiction movies. Once you have this table, you can merge the movies table in to see the movie names. Since this exercise is related to science fiction movies, use a right join as your superhero power to solve this problem."""
+
+The movies, scifi_movies, and action_movies tables have been loaded for you.
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+# Merge the movies and scifi_only tables with an inner join
+movies_and_scifi_only = movies.merge(scifi_only,left_on='id',right_on='movie_id')
+
+# Print the first few rows and shape of movies_and_scifi_only
+print(movies_and_scifi_only.head())
+print(movies_and_scifi_only.shape)
