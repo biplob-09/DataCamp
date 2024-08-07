a58494f6611cbe02106b17ba10a03a498e451433
Biplob Nath <99787352+biplob-09@users.noreply.github.com>
2024-08-07 23:52:48 +0530

    Update merging_tables.py

--- merging_tables.py
+++ merging_tables.py
@@ -1,3 +1,5 @@
+'https://rpubs.com/datttrian/joining-data-with-pandas'
+
 """merge three dataframes"""
 licenses_zip_ward = licenses.merge(zip_demo,on= 'zip') \
             			.merge(wards,on='ward')
@@ -60,7 +62,7 @@ titles_diff = orig_seq[['title_org','title_seq','diff']]
 print(titles_diff.sort_values('diff',ascending=False).head())
 
 """Inner Join"""
- Merge the non_mus_tcks and top_invoices tables on tid
+"""Merge the non_mus_tcks and top_invoices tables on tid"""
 tracks_invoices = non_mus_tcks.merge(top_invoices, on='tid')
 
 # Use .isin() to subset non_mus_tcks to rows with tid in tracks_invoices
@@ -71,3 +73,20 @@ cnt_by_gid = top_tracks.groupby(['gid'], as_index=False).agg({'tid':'count'})
 
 # Merge the genres table to cnt_by_gid on gid and print
 print(cnt_by_gid.merge(genres,on='gid'))
+
+
+"""concat and merge"""
+# Concatenate the classic tables vertically
+classic_18_19 = pd.concat([classic_18, classic_19], ignore_index=True)
+
+# Concatenate the pop tables vertically
+pop_18_19 = pd.concat([pop_18, pop_19], ignore_index=True)
+
+# Merge classic_18_19 with pop_18_19
+classic_pop =  classic_18_19.merge(pop_18_19, on='tid')
+
+# Using .isin(), filter classic_18_19 rows where tid is in classic_pop
+popular_classic = classic_18_19[classic_18_19['tid'].isin(classic_pop['tid'])]
+
+# Print popular chart
+print(popular_classic)
